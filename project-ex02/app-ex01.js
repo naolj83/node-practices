600237d6345185612425c7a7b1363f1e817e07ad
naolj83 <naolj83@gmail.com>
2021-07-09 12:55:29 +0900

    모듈 테스트

---
+++ project-ex02/app-ex01.js
@@ -0,0 +1,9 @@
+/**
+ * naolj83-math.js 모듈 테스트ㅜㅜㅐ
+ */
+
+const dzMath = require('../naolj83-math/naolj83-math');
+
+console.log(dzMath.sum(1, 2, 3, 4));
+console.log(dzMath.max(-10, -20, -30, -40));
+console.log(dzMath.min(-1, -2, 0, 1, 2));
